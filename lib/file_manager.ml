3ebdb4b7a6aa287fc3737f7cf88daf644907ce46
bglid <benjamin.h.glidden@gmail.com>
2026-03-20 16:08:21 -0400

    First pass adding block and page to filemanager

---
+++ lib/file_manager.ml
@@ -0,0 +1,42 @@
+module Block = struct
+  type t = string * int
+
+  let file_name (filename, _) = filename
+  let block_num (_, blocknum) = blocknum
+  let make ~filename ~block_num = (filename, block_num)
+  let to_string (filename, blocknum) = Printf.sprintf "%s, %d" filename blocknum
+  let eq (fn1, bn1) (fn2, bn2) = fn1 = fn2 && bn1 = bn2
+end
+
+module Page = struct
+  type t = bytes
+
+  let make ~block_size = Bytes.make block_size '\000'
+  let from_bytes b = b
+  let get_int32 page offset = Bytes.get_int32_ne page offset
+  let set_int32 page offset b32 = Bytes.set_int32_ne page offset b32
+  let contents page = page
+
+  let get_bytes page offset =
+    let len = Int32.to_int (get_int32 page offset) in
+    Bytes.sub page (offset + 4) len
+
+  let set_bytes page offset b =
+    assert (offset + 4 + Bytes.length b <= Bytes.length page);
+    let len = Bytes.length b in
+    let _ = set_int32 page offset (Int32.of_int len) in
+    Bytes.blit b 0 page (offset + 4) len
+
+  let get_string page offset = Bytes.to_string (get_bytes page offset)
+  let set_string page offset s = set_bytes page offset (Bytes.of_string s)
+
+  let set_string_raw page offset s =
+    let b = Bytes.of_string s in
+    let len = Bytes.length b in
+    Bytes.blit b 0 page offset len
+
+  let get_string_raw page offset n = Bytes.to_string (Bytes.sub page offset n)
+  let max_len offset = offset + 4
+  let ( = ) = Bytes.equal
+  let zero_out page = Bytes.fill page 0 (Bytes.length page) '\000'
+end
---
+++ lib/file_manager.mli
@@ -0,0 +1,76 @@
+(** The Filemanager is how we parse through multiple files in our B+ tree
+    storage implementation. *)
+
+(** Blocks are representative of the Blocks on the Disk *)
+module Block : sig
+  type t
+
+  val file_name : t -> string
+  (** Return the file name upon giving the block*)
+
+  val block_num : t -> int
+  (** Get the block offset, as an int, in the file given the block id *)
+
+  val make : filename:string -> block_num:int -> t
+  (** Init block string and int here: Take a labeled arg ~filename, which is a
+      string, and an arg ~block_num, which is an int, to create a Block type t.
+      Using labels for clarity in API design here*)
+
+  val to_string : t -> string
+  (** Converts Block Id to string representation *)
+
+  val eq : t -> t -> bool
+  (** Boolean check on if blocks are the same *)
+end
+
+(** Page is a module that allows us to buffer bytes in memory. It allows us to
+    do work on the blocks before writing them back to disk*)
+module Page : sig
+  type t
+
+  val make : block_size:int -> t
+  (** Calls Bytes.make to init a block of size ~block_size and returns an init
+      page. Uses null char '\000' *)
+
+  val from_bytes : bytes -> t
+  (** Return page from bytes *)
+
+  val get_int32 : t -> int -> int32
+  (** Given page t, and offset in int, return the 32-bit integer that page t
+      belongs to, via Bytes.get_int32_ne *)
+
+  val set_int32 : t -> int -> int32 -> unit
+  (** Given page t, and offset in int, and a int32, set page in the 32-bit
+      integer, from the offset to the passed int32, via Bytes.set_int32_ne *)
+
+  val contents : t -> bytes
+  (** Return the bytes given page t *)
+
+  val get_bytes : t -> int -> bytes
+  (** Using page t and offset int, get the bytes back *)
+
+  val set_bytes : t -> int -> bytes -> unit
+  (** Using page t, offset int, and bytes b, get the bytes back *)
+
+  val get_string : t -> int -> string
+  (** With page and offset, return the bytes in string. Uses Bytes.to_string *)
+
+  val set_string : t -> int -> string -> unit
+  (** With page offset and the string, Set bytes using Bytes.of_string *)
+
+  val set_string_raw : t -> int -> string -> unit
+  (** Gvne page, offset and the string, get bytes of the string and set using
+      Bytes.blit*)
+
+  val get_string_raw : t -> int -> int -> string
+  (** Get the raw string of length n using Bytes.sub *)
+
+  val max_len : int -> int
+  (**  *)
+
+  val ( = ) : t -> t -> bool
+  (** Wrapper for Bytes.equal *)
+
+  val zero_out : t -> unit
+  (** Fill page t with 0 using Bytes.fill *)
+end
